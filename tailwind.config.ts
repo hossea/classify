21fa04fd27896af2a584ca6172641867189ed526
hossea <hosseamulama@gmail.com>
2024-06-01 17:14:52 +0300

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'dark-blue': '#14044b',
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
+
